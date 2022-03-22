5280411a37f16fdcab69d46065e648af4c5793df
jaebong8 <jaebong8@github.com>
2022-03-23 00:09:53 +0900

    audio tag muted 추가

--- js/main.js
+++ js/main.js
@@ -66,6 +66,7 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 brandDiv.addEventListener("click", () => {
+  brandAudio.muted = false;
   brandAudio.play();
 });
 brandDiv.addEventListener("mouseleave", () => {
