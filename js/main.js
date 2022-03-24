b05918923f9c8b3daa4c3a363f0d6e9eb15c3ce7
jaebong8 <jaebong8@github.com>
2022-03-24 17:20:53 +0900

    scroll action 추가

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ const brandAudio = brandDiv.querySelector("audio");
 const works = document.querySelector("#works");
 const lists = works.querySelectorAll("ul li");
 const wraps = works.querySelectorAll(".wrap2");
+// scroll
+const scrollSections = document.querySelectorAll(".scroll_section");
+const scroll_nav = document.querySelector(".scroll_nav");
+const scroll_lis = scroll_nav.querySelectorAll("li");
+let heightArr = [];
+let base = -50;
+let enableClick = true;
 
 //visual auto typing
 const visualH2 = document.querySelector("#visual .inner h2");
@@ -21,7 +28,7 @@ const TypingStr = strTxt.split("");
 // 스와이퍼
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  speed: 500,
+  speed: 1000,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -56,6 +63,76 @@ brandDiv.addEventListener("mouseleave", () => {
   brandAudio.pause();
 });
 
+lists.forEach((el, index) => {
+  el.addEventListener("click", (e) => {
+    e.preventDefault();
+    const isOn = el.classList.contains("on");
+    if (isOn) return;
+    for (list of lists) {
+      list.classList.remove("on");
+    }
+    el.classList.add("on");
+
+    wraps.forEach((wrap) => {
+      wrap.classList.remove("on");
+    });
+    wraps[index].classList.add("on");
+  });
+});
+
+// 스크롤 액션 --------------------------------------
+setHeight();
+window.addEventListener("resize", setHeight);
+scroll_lis.forEach((li, index) => {
+  li.addEventListener("click", (e) => {
+    let isOn = e.currentTarget.classList.contains("on");
+    if (isOn) return;
+    if (enableClick) {
+      enableClick = false;
+      moveScroll(index);
+    }
+  });
+});
+
+window.addEventListener("scroll", activation);
+
+function setHeight() {
+  heightArr = [];
+  scrollSections.forEach((section, index) => {
+    heightArr.push(section.offsetTop);
+  });
+  const active = scroll_nav.querySelector("li.on");
+  const activeIndex = Array.from(scroll_lis).indexOf(active);
+  window.scroll(0, heightArr[activeIndex]);
+}
+
+function moveScroll(index) {
+  new Anime("window", {
+    prop: "scroll",
+    value: heightArr[index],
+    duration: 500,
+    callback: () => {
+      enableClick = true;
+    },
+  });
+}
+function activation() {
+  let scroll = window.scrollY || window.pageYOffset;
+
+  scrollSections.forEach((section, index) => {
+    if (scroll >= heightArr[index] + base) {
+      scroll_lis.forEach((el) => {
+        el.classList.remove("on");
+      });
+      scroll_lis[index].classList.add("on");
+      for (let section of scrollSections) {
+        section.classList.remove("on");
+      }
+      scrollSections[index].classList.add("on");
+    }
+  });
+}
+
 // 오토 타이핑
 function Typing() {
   setTimeout(() => {
@@ -81,20 +158,3 @@ function Typing() {
     }, 100);
   });
 }
-
-lists.forEach((el, index) => {
-  el.addEventListener("click", (e) => {
-    e.preventDefault();
-    const isOn = el.classList.contains("on");
-    if (isOn) return;
-    for (list of lists) {
-      list.classList.remove("on");
-    }
-    el.classList.add("on");
-
-    wraps.forEach((wrap) => {
-      wrap.classList.remove("on");
-    });
-    wraps[index].classList.add("on");
-  });
-});
