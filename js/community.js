bd834f95e927a3e2b2b93c3844bcfa83f8eb0f6f
jaebong8 <jaebong8@github.com>
2022-03-25 16:41:14 +0900

    news 컨텐츠 추가

--- js/community.js
+++ js/community.js
@@ -1,14 +1,17 @@
 const frame = document.querySelector(".community");
-const moreBtn = frame.querySelector(".fa-plus");
+const moreBtns = frame.querySelectorAll(".fa-plus");
 
-moreBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const detail = e.currentTarget.closest("div").nextElementSibling;
-
-  detail.classList.toggle("on");
-  if (detail.classList.contains("on")) {
-    moreBtn.className = "fas fa-minus";
-  } else {
-    moreBtn.className = "fas fa-plus";
-  }
+moreBtns.forEach((moreBtn) => {
+  moreBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const detail = e.currentTarget.closest("div").nextElementSibling;
+    const short = e.currentTarget.closest(".short");
+    short.classList.toggle("on");
+    detail.classList.toggle("on");
+    if (detail.classList.contains("on")) {
+      moreBtn.className = "fas fa-minus";
+    } else {
+      moreBtn.className = "fas fa-plus";
+    }
+  });
 });
